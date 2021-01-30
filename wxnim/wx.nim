7cb1fcca452c2a1ad95afed5c5846f2e8a31a402
Joubert Nel <joubert@joubster.com>
2021-01-30 10:11:25 -0800

    explains WxString to String converter

--- wxnim/wx.nim
+++ wxnim/wx.nim
@@ -20,7 +20,21 @@ include private/string
 converter toWxString*(s: string): WxString =
   result = constructWxString(cstring(s), s.len)
 
-converter toString*(s: WxString): string = 
+converter toString*(s: WxString): string =
+  #[
+    Context about wxString handling:
+        - wxString class reference, particularly .ToUTF8() method 
+          https://docs.wxwidgets.org/3.0/classwx_string.html   
+        - wxScopedCharTypeBuffer<T> reference, particularly .data() method 
+          https://docs.wxwidgets.org/3.0/classwx_scoped_char_type_buffer.html#a7cd7ba0ab32e9f63779f602c2bdfd9b8
+    
+    Nim emit pragma for embedding C++ code:
+        - https://nim-lang.github.io/Nim/manual.html#implementation-specific-pragmas-emit-pragma
+        
+    C++11 limits on implicit conversion from const char* to char* 
+        - https://stackoverflow.com/questions/48554625/vs-2017-doesnt-implicitly-convert-const-char-to-char/48554786
+        - https://en.cppreference.com/w/cpp/language/const_cast
+  ]# 
   var r: cstring
 
   {.emit: """
